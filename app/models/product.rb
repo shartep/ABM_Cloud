c4cb7850081517be736928e6d5d11c90503000c7
Alex Petrash <alexp@elephantheadsoft.com>
2015-12-03 17:04:01 +0200

    implement UI for products list page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  belongs_to :supplier, foreign_key: :supplier_code, primary_key: :code
+
   validates :sku, uniqueness: true, presence: true
   validates :supplier_code, presence: true
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,5 @@
 class Supplier < ActiveRecord::Base
+  has_many :products, foreign_key: :supplier_code, primary_key: :code
+
   validates :code, uniqueness: true, presence: true
 end
